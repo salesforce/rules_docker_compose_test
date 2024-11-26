878ac222c9632d97e92719715345c5b20398d764
Kris Foster <kfoster@salesforce.com>
2024-11-26 11:02:36 +0000

    update

--- docker_compose_test/test_container_entrypoint.sh
+++ docker_compose_test/test_container_entrypoint.sh
@@ -35,6 +35,10 @@ for JAR in $JARS; do
   CLASS_PATH_STRING="$CLASS_PATH_STRING --class-path $JAR"
 done
 
+echo "foo"
+echo $JAVA_HOME
+echo $PATH
+
 # JVM_ARGS can be set in the docker-compose file.
 # JUNIT_PARAMS can be set in the docker-compose file to filter for specific tests
 # e.g. --include-classname com.something.integration.*
