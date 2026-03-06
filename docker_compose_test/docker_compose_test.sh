d66aefe8ae7329c4b918b474f28c32d649737705
Kris foster <kfoster@salesforce.com>
2026-03-06 12:15:11 +0000

    Improve exit code handling for Docker tests
    Refactor exit code retrieval for Docker container.

--- docker_compose_test/docker_compose_test.sh
+++ docker_compose_test/docker_compose_test.sh
@@ -69,10 +69,12 @@ trap cleanup EXIT
 docker_compose_up_cmd="$docker_compose_bin -f $ABSOLUTE_COMPOSE_FILE_PATH up --exit-code-from $DOCKER_COMPOSE_TEST_CONTAINER $EXTRA_DOCKER_COMPOSE_UP_ARGS"
 echo "running: $docker_compose_up_cmd"
 echo "$docker_compose_up_cmd" | bash
-result=$?
 
 # Figure out the exit code of the test container incase it never actually started.
-EXIT_CODE=$(docker inspect $($docker_compose_bin -f $ABSOLUTE_COMPOSE_FILE_PATH ps -qa $DOCKER_COMPOSE_TEST_CONTAINER) --format='{{.State.ExitCode}}' 2>/dev/null)
+docker_inspect_cmd="$(docker inspect $($docker_compose_bin -f $ABSOLUTE_COMPOSE_FILE_PATH ps -qa $DOCKER_COMPOSE_TEST_CONTAINER) --format='{{.State.ExitCode}}' 2>/dev/null)"
+echo "running: $docker_inspect_cmd"
+echo "$docker_inspect_cmd" | bash
+EXIT_CODE=$?
 if [ "$EXIT_CODE" != "0" ] || [ -z "$EXIT_CODE" ]; then
   echo "Error: $DOCKER_COMPOSE_TEST_CONTAINER container failed or never started!"
   exit 1
